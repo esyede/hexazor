3fbbe20a6adf8fb7af4a49e6c68a0a325ca093ea
Suyadi <suyadi.1992@gmail.com>
2019-12-30 01:34:00 +0700

    :traffic_light: begin to add unit testing

---
+++ tests/system/core/ApplicationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use System\Core\Config;
+use System\Core\Router;
+use System\Debugger\Debugger;
+
+class ApplicationTest extends TestCase
+{   
+    public function __construct()
+    {
+        parent::__construct();
+        require_once dirname(dirname(dirname(__DIR__))).'/index.php';
+    }
+
+    public function testEnsureAppKeyIsProvided()
+    {
+        return $this->assertTrue(strlen(Config::get('app.application_key')) === 32);
+    }
+
+    public function testReconfigureTimezone()
+    {
+        return $this->assertSame(date_default_timezone_get(), 'Asia/Jakarta');
+    }
+
+    public function testReconfigureDebugger()
+    {
+        return $this->assertTrue(Debugger::isEnabled());
+    }
+
+    public function testRegisterDefinedFacades()
+    {
+        $this->assertTrue(is_string(Date::now()->get()));
+    }
+
+    public function testDispatchRouteDefinitions()
+    {
+        return $this->assertTrue(is_array(Router::getRoutes()));
+    }
+
+    public function __destruct()
+    {
+        // ..
+    }
+}
---
+++ tests/system/core/ConfigTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use System\Core\Config;
+
+class ConfigTest extends TestCase
+{   
+    public function __construct()
+    {
+        parent::__construct();
+        require_once dirname(dirname(dirname(__DIR__))).'/index.php';
+    }
+
+    public function testInit()
+    {
+        return $this->assertInstanceOf(Config::class, Config::init());
+    }
+
+    public function testGet()
+    {
+        return $this->assertSame(Config::get('session.lifetime'), 3600);
+    }
+
+    public function testSet()
+    {
+        Config::set('session.lifetime', 8000);
+
+        return $this->assertSame(Config::get('session.lifetime'), 8000);
+    }
+
+    public function testHas()
+    {
+        return $this->assertTrue(Config::has('session.lifetime'));
+    }
+
+    public function testAll()
+    {
+        $all = Config::all();
+        
+        return $this->assertTrue(is_array($all) && count($all) > 5);
+    }
+
+    public function __destruct()
+    {
+        // ..
+    }
+}
