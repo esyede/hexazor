e66c508375bf63c2c0484da0de3b3a94ffd5ba14
Suyadi <suyadi.1992@gmail.com>
2020-01-02 04:27:37 +0700

    :grin: can test php7.4 since i dont have 64bit machine

--- system/Debugger/Debugger.php
+++ system/Debugger/Debugger.php
@@ -171,15 +171,16 @@ class Debugger
         }
 
         $error = error_get_last();
+        $errList = [
+            E_ERROR,
+            E_CORE_ERROR,
+            E_COMPILE_ERROR,
+            E_PARSE,
+            E_RECOVERABLE_ERROR,
+            E_USER_ERROR,
+        ];
 
-        if (in_array($error['type'], [
-            \E_ERROR,
-            \E_CORE_ERROR,
-            \E_COMPILE_ERROR,
-            \E_PARSE,
-            \E_RECOVERABLE_ERROR,
-            \E_USER_ERROR
-        ], true)) {
+        if (in_array($error['type'], $errList, true)) {
             self::exceptionHandler(
                 Helpers::fixStack(
                     new \ErrorException(
