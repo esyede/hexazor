f9669df705a2a0a4c3665eea220fd3ba504b8769
Suyadi <suyadi.1992@gmail.com>
2020-01-02 15:07:56 +0700

    :grin: small changes

--- constants.php
+++ constants.php
@@ -2,16 +2,16 @@
 
 // misc.
 define('FRAMEWORK_START', microtime(true));
-define('DS',              DIRECTORY_SEPARATOR);
-define('VERSION',         '0.9.2');
+define('DS', DIRECTORY_SEPARATOR);
+define('VERSION', '0.9.2');
 
 // paths
-define('BASE_PATH',       realpath(__DIR__).DS);
-define('SYSTEM_PATH',     BASE_PATH.'system'.DS);
-define('APP_PATH',        BASE_PATH.'app'.DS);
-define('STORAGE_PATH',    BASE_PATH.'storage'.DS);
-define('DATABASE_PATH',   BASE_PATH.'database'.DS);
-define('RESOURCE_PATH',   BASE_PATH.'resources'.DS);
+define('BASE_PATH', realpath(__DIR__).DS);
+define('SYSTEM_PATH', BASE_PATH.'system'.DS);
+define('APP_PATH', BASE_PATH.'app'.DS);
+define('STORAGE_PATH', BASE_PATH.'storage'.DS);
+define('DATABASE_PATH', BASE_PATH.'database'.DS);
+define('RESOURCE_PATH', BASE_PATH.'resources'.DS);
 define('CONTROLLER_PATH', APP_PATH.'Controllers'.DS);
-define('VIEW_PATH',       RESOURCE_PATH.'views'.DS);
-define('UPLOADS_PATH',    STORAGE_PATH.'app'.DS.'uploads'.DS);
+define('VIEW_PATH', RESOURCE_PATH.'views'.DS);
+define('UPLOADS_PATH', STORAGE_PATH.'app'.DS.'uploads'.DS);
